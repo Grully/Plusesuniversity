72128215c1be94488dfce85f091741b9ece3fc90
Ilya Voronov <80422475+Grully@users.noreply.github.com>
2022-10-31 18:27:26 +0300

    updated

--- MatrixSaddlePoint.cpp
+++ MatrixSaddlePoint.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using std::cout;
 using std::cin;
 using std::endl;
@@ -22,14 +22,35 @@ int main()
         }
     }
     int maxinstring[3];
-    int maxinpillar[4];
+    int maxincolumn[4];
+    int max = 0;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 4; j++) {
-            cout << matrix[i][j] << " ";
-            if (j == 3) {
-                cout << endl;
+            if(j == 0) {
+                max = matrix[i][j];
+            }
+            if(max <= matrix[i][j]) {
+                max = matrix[i][j];
+            }
+            if(j == 3){
+                maxinstring[i] = max;
             }
         }
     }
-
+    
+    max = 0;
+    
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 3; j++) {
+            if(j == 0) {
+                max = matrix[j][i];
+            }
+            if(max <= matrix[j][i]) {
+                max = matrix[j][i];
+            }
+            if(j == 2){
+                maxincolumn[i] = max;
+            }
+        }
+    }
 }
